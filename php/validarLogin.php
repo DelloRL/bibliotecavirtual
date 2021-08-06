ebfb45d6d6359d61c504c51b7f3ae1b5b1a5d33d
DelloRL <lucas.dello.russo@gmail.com>
2021-08-05 23:21:18 -0300

    Agrego php completo y corrijo biblioteca.css

--- php/validarLogin.php
+++ php/validarLogin.php
@@ -5,3 +5,42 @@ $host = "localhost";
 
 $connection = mysqli_connect($host, $user, $pass);
 
+$user = $_POST["usuario"];
+print 'El usuario es' . $user . '<br>';
+$contrasena = $_POST["contrasena"];
+print 'La contraseña es ' . $contrasena . '<br>';
+
+if (!$connection){
+    echo "No se ha podido conectar con el servidor" . mysqli_error();
+}
+else {
+    echo "Hemos conectado al servidor <br>";
+}
+
+$database = "bibliotecavirtual";
+
+$db = mysqli_select_db($connection,$database);
+
+if(!$db){
+    echo "No se ha podido encontrar la base de datos <br>";
+}
+else {
+    echo "Tabla seleccionada <br>";
+    $consulta = "SELECT * FROM USUARIO where NombreUsuario = '".$user."' and ContraseñaUsuario = '".$contrasena."' ";
+    echo $consulta . "<br>";
+    $result = mysqli_query($connection,$consulta);
+    $fila = mysqli_fetch_array($result);
+}
+
+if(mysqli_num_rows($result)!=1){
+    echo "Usuario no válido. <br>";
+}
+else{
+    $cUsuario = $fila['NombreUsuario'];
+    echo "Bienvenido " . $cUsuario;
+    header('Locatiion: menu.php');
+}
+
+mysqli_close( $connection );
+
+?>
