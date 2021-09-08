8a3e1083eba3fcec725e8117fcf2489392a97b25
DelloRL <lucas.dello.russo@gmail.com>
2021-09-08 15:00:02 -0300

    Actualizo

--- php/register.php
+++ php/register.php
@@ -4,24 +4,23 @@ $user = "root";
 $pass = "";
 $host = "localhost";
 $database = "bibliotecavirtual";
-
 $connection = mysqli_connect($host, $user, $pass);
 
-if (isset($_POST['submit'])) {
-    $user = $_POST ['usuario'];
-    $email = $_POST ['email'];
-    $password = md5($_POST ['contrasena']);
-    $cpassword = md5($_POST ['ccontrasena']);
 
-    if($password = $cpassword) {
-        $sql = "INSERT INTO USUARIO (NombreUsuario, emailUsuario, ContraseñaUsuario)
-                VALUES ('$user', '$email', '$password')";
-        $result = mysqli_query($connection, $sql);
-        if(!$result) {
-            echo "Algo saliò mal"
+if (isset($_POST['register'])){
+    if (strlen($_POST['txtNombre']) >= 1 && strlen($_POST['txtUsuario']) >= 1 && strlen($_POST['txtContraseña']) >= 1 && strlen($_POST['txtCorreo']) >= 1) {
+        $name = $_POST['txtNombre'];
+        $username = $_POST['txtUsuario'];
+        $password = $_POST['txtContraseña'];
+        $email = $_POST['txtCorreo'];
+        $consulta = "INSERT INTO usuario(NombreUsuario, ContraseñaUsuario, NombreCompleto, emailUsuario) VALUES ('$name','$username','$password','$email')";
+        $result = mysqli_query($connection,$consulta);
+        if ($result){
+            header('Location: paginaPrincipal.php');
         } else {
-            echo "La contraseña no coincide"
+            echo "Algo salió mal.";
         }
+    }
 }
-}
+
 ?>
